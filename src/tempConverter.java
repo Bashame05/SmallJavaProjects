13e880dbf7a055d50a8c6684e0193c624009ec62
Bashame05 <makizenin1601@gmail.com>
2025-07-01 20:48:36 +0530

    Temp converter

---
+++ src/tempConverter.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class tempConverter {
+    public static void main(String[] args){
+        Scanner bleh = new Scanner(System.in);
+        double temp;
+        double newtemp;
+        String unit;
+
+        System.out.print("Enter a temperature you wanna convert: ");
+        temp = bleh.nextDouble();
+
+        System.out.print("Do you wanna convert it to celsius or fahrenheit ? (C/F): ");
+        unit = bleh.next().toUpperCase();
+
+        newtemp = (unit.equals("F")) ? (temp-32)*5/9 : (temp*5/9)+32;
+
+        System.out.printf("%.2f°%s",newtemp,unit);
+
+    }
+}

de1a467acfd7f0e426bddc27c03aad3017833e70
Bashame05 <makizenin1601@gmail.com>
2025-12-22 10:54:44 +0530

    added exception handling for choice input

--- src/todolist/ToDoList.java
+++ src/todolist/ToDoList.java
@@ -1,11 +1,12 @@
 package todolist;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ToDoList {
     static boolean isRunning=true;
-    static ArrayList<String> tasks = new ArrayList<>();
+    static ArrayList<Task> tasks = new ArrayList<>();
     static Scanner bleh = new Scanner(System.in);
     public static void main(String[] args) {
         while(isRunning){
@@ -16,10 +17,18 @@ public class ToDoList {
             System.out.println("       MENU      ");
             System.out.println("1:ADD A TASK");
             System.out.println("2:VIEW TASK");
-            System.out.println("3:EXIT");
-            System.out.println("Enter your choice(1-3):");
-            int choice = bleh.nextInt();
-            bleh.nextLine();
+            System.out.println("3:MARK AS DONE");
+            System.out.println("4:EXIT");
+            int choice;
+            try {
+                System.out.print("Enter your choice(1-3):");
+                choice = bleh.nextInt();
+                bleh.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println("Enter a valid choice ");
+                bleh.nextLine();
+                continue;
+            }
 
             switch(choice){
                 case 1:
@@ -29,7 +38,10 @@ public class ToDoList {
                     viewTask();
                     break;
                 case 3:
-                    isRunning=false;
+                    markTask();
+                    break;
+                case 4:
+                    isRunning = false;
                     break;
                 default:
                     System.out.println("ENTER A VALID CHOICE!!!");
@@ -38,18 +50,34 @@ public class ToDoList {
         }
     }
 
+    private static void markTask() {
+        System.out.print("Enter which number of task to mark: ");
+        int id  = bleh.nextInt()-1;
+        bleh.nextLine();
+        Task task = tasks.get(id);
+        if(task.getStatus()!= STATUS.COMPLETED) {
+            task.markCompleted();
+            System.out.println("***************");
+            System.out.println("TASK COMPLETED");
+        }else{
+            System.out.println("Task is already completed");
+        }
+    }
+
     private static void viewTask() {
         int idx=1;
-        for (String task : tasks) {
-            System.out.println(idx + ":" + task);
+        System.out.println("***************");
+        for (Task task : tasks) {
+            System.out.println(idx + ": [" + task.getStatus() + "]" + task.getTask());
             idx++;
         }
     }
 
     private static void addTask() {
-        System.out.println("Enter your task:");
+        System.out.println("***************");
+        System.out.print("Enter your task:");
         String task = bleh.nextLine();
-        tasks.add(task);
+        tasks.add(new Task(task));
         System.out.println("TASK ADDED");
     }
 }
